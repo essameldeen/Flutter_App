52d58c38b5cce710ca3c815229d3ce4f2d2b49ca
esammohamed <esammohamed17121996@gmail.com>
2023-03-13 11:48:29 +0200

    Fcm and handling it

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:app_test/layout/social_app/cubit/cubit.dart';
 import 'package:app_test/layout/social_app/social_layout.dart';
 import 'package:app_test/modules/social_app/social_login/social_login_screen.dart';
 import 'package:app_test/shared/block_0bserver.dart';
+import 'package:app_test/shared/components/components.dart';
 import 'package:app_test/shared/components/constants.dart';
 import 'package:app_test/shared/cubit/cubit.dart';
 import 'package:app_test/shared/cubit/states.dart';
@@ -10,17 +11,52 @@ import 'package:app_test/shared/network/local%20/cach_helper.dart';
 import 'package:app_test/shared/network/remote/dio_helper.dart';
 import 'package:app_test/shared/styles/themes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'layout/news_app/cubit/cubit.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+
+  print(" on background message");
+  print(message.data);
+  print(message.notification.toString());
+  showToast("on message background ", ToastStates.SUCCESS);
+}
+
 
 void main() async {
   WidgetsFlutterBinding
       .ensureInitialized(); // to ensure that all function finished before running
   Bloc.observer = MyBlocObserver();
   await CacheHelper.init();
-  await Firebase.initializeApp();
+
+   await Firebase.initializeApp();
+
+  var tokenApp = await FirebaseMessaging.instance.getToken();
+
+  FirebaseMessaging.onMessage.listen((event) {
+    print("hena");
+    print(event.data.toString());
+    print(event.notification.toString());
+
+    showToast("on message", ToastStates.SUCCESS);
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    print("hena on background");
+    print(event.data.toString());
+    print(event.notification.toString());
+    showToast("on message open app ", ToastStates.SUCCESS);
+
+  });
+
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+
+   print(tokenApp);
   DioHelper.init();
 
   Widget? screen;
@@ -57,7 +93,9 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => SocialCubit()..getUserData()..getPosts(),
+          create: (context) => SocialCubit()
+            ..getUserData()
+            ..getPosts(),
         ),
         BlocProvider(
           create: (context) => NewsCubit()..getBusinessData(),
